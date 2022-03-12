c62f5cc6086459e7382fce3276dbc31b7cb39c20
Naufal Azim <naufal.azim@outlook.com>
2022-03-12 15:00:58 +0800

    initial setup spring boot

---
+++ src/test/java/com/Tracker/Satellite/SatelliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tracker.Satellite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatelliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
